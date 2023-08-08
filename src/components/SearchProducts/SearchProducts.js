8d52382150274a80e53256af7e80c464ae9884c6
AngieCristina <angiecristinagonzalessara@gmail.com>
2023-08-08 11:28:49 -0500

    add logic for searchProducts

--- src/components/SearchProducts/SearchProducts.js
+++ src/components/SearchProducts/SearchProducts.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function SearchProducts() {
-  return <div></div>;
+function SearchProducts({ onSearch }) {
+  const [searchProduct, setSearchProduct] = useState('');
+
+  const handleChange = event => {
+    setSearchProduct(event.target.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSearch(searchProduct);
+    console.log(searchProduct);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchProduct} onChange={handleChange} />
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchProducts;
